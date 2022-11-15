4191ac5a8aa3bad82983fbf99ae8233def3907cf
Beatriz Fontes <fontesbeatriz26@gmail.com>
2022-11-14 22:58:23 -0300

    Inserindo método distância

--- ex2/Program.cs
+++ ex2/Program.cs
@@ -7,12 +7,16 @@ namespace ex2
     {
         static void Main(string[] args)
         {
-            Vertice v1 = new Vertice();
-            v1.posicaox = 0;
-            v1.posicaoy = 1;
+            Vertice v1 = new Vertice(2,3);
+            Vertice v2 = new Vertice(1,1);
 
-            Console.WriteLine("A posição do vértice é: ({0},{1})", v1.posicaox, v1.posicaoy);
-            Console.ReadLine(); 
+
+            float distancia = v1.distancia(v1.getx, v1.gety, v2.getx, v2.gety);
+
+            Console.WriteLine("A posição do vértice 1 é: ({0};{1})", v1.getx, v1.gety);
+            Console.WriteLine("A posição do vértice 2 é: ({0};{1})", v2.getx, v2.gety);
+            Console.WriteLine("A Distância euclidiana entre os 2 pontos é: {0}", distancia);
+            Console.ReadLine();
         }
     }
 }
--- ex2/Vertice.cs
+++ ex2/Vertice.cs
@@ -7,20 +7,37 @@ namespace ex2
         private float x;
         private float y;
 
-        public float posicaox
+        private float setx
         {
-            get { return x; }
             set { x = value; }
         }
-        public float posicaoy
+        public float getx
         {
-            get { return y; }
+            get { return x; }
+        }
+        private float sety
+        {
             set { y = value; }
         }
-
+        public float gety
+        {
+            get { return y; }
+        }
         public Vertice() {
-            this.x = 0;
-            this.y = 0;
+            this.setx = 0;
+            this.sety = 0;
+        }
+        public Vertice(float x, float y)
+        {
+            this.setx = x;
+            this.sety = y;
+        }
+
+        public float distancia(float x1, float y1, float x2, float y2) {
+            float d, dist;
+            d = (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
+            dist = (float)Math.Sqrt(d);
+            return dist;
         }
     }
 }
